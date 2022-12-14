0cd26ba50aea8934a190b58d87f695d0a0f1f8af
Kseniya Elsukova <kseniyaels@yandex.ru>
2022-12-14 15:51:13 +0300

    Задача 5

---
+++ Задача5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum =  0;
+
+while (number > 0)
+{
+    sum = sum + number % 10;
+    number /=  10;
+}
+
+Console.WriteLine("Сумма равна {0}", sum);
